3ef0e48f47ebaf1d5d92497df1904eccc7cbdc3d
Juan Carlos Baez Diaz <146070006+Juancar17@users.noreply.github.com>
2024-12-10 23:23:42 +0100

    Delete portafolio directory

--- portafolio/src/components/Helmet.jsx
+++
@@ -1,8 +0,0 @@
-import { Helmet } from "react-helmet";
-<Helmet>
-  <title>Portafolio | Juancar Dev</title>
-  <meta
-    name="description"
-    content="Portafolio de Juan Carlos, desarrollador Full-Stack."
-  />
-</Helmet>;
